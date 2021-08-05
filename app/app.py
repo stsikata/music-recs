8a568c1ad36db88b33decea56d46ec099202036d
Sedina <sat9978l@nyu.edu>
2021-08-05 17:01:50 -0400

    Input username

--- app/app.py
+++ app/app.py
@@ -10,34 +10,64 @@ clients_secret = os.getenv("SPOTIPY_CLIENT_SECRET")
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 
+import requests
+import json
+
 # client_credentials_manager = SpotifyClientCredentials(client_id=clients_id, client_secret=clients_secret)
 # sp = spotipy.Spotify(client_credentials_manager)
 
+# urn = "spotify:artist:3jOstUTkEu2JkjvRdBA5Gu"
 auth_manager = SpotifyClientCredentials(client_id=clients_id, client_secret=clients_secret)
 sp = spotipy.Spotify(auth_manager=auth_manager)
 
-playlists = sp.user_playlists('spotify')
-while playlists:
-    for i, playlist in enumerate(playlists['items']):
-        print("%4d %s %s" % (i + 1 + playlists['offset'], playlist['uri'],  playlist['name']))
-    if playlists['next']:
-        playlists = sp.next(playlists)
-    else:
-        playlists = None
+# artist = sp.artist(urn)
+# print("KEYS:", artist.keys())
+# print("ARTIST:", artist)
 
-# artist_name = []
-# track_name = []
-# popularity = []
-# track_id = []
-# for i in range(0,10000,50):
-#     track_results = sp.search(q='year:2018', type='track', limit=50,offset=i)
-#     for i, t in enumerate(track_results['tracks']['items']):
-#         artist_name.append(t['artists'][0]['name'])
-#         track_name.append(t['name'])
-#         track_id.append(t['id'])
-#         popularity.append(t['popularity'])
+#### Attempting to access API
 
-# import pandas as pd
-# track_dataframe = pd.DataFrame({'artist_name' : artist_name, 'track_name' : track_name, 'track_id' : track_id, 'popularity' : popularity})
-# print(track_dataframe.shape)
-# track_dataframe.head()
+# request_url = "https://accounts.spotify.com/authorize"
+# requests_url = "https://accounts.spotify.com/api/token"
+# response = requests.get(request_url)
+# print(type(response))
+# print(response.status_code) # the part of http response that lets us know how successful
+# print(response.text) # actual body of the response
+
+# print("---------")
+
+# responses = requests.get(requests_url)
+# print(type(responses))
+# print(responses.status_code) # the part of http response that lets us know how successful
+# print(responses.text) # actual body of the response
+
+# shows artist info for a URN or URL
+
+import sys
+import pprint
+import random
+
+print("~~~~~~~~~~~~~~~~~~~~~~~~~")
+
+if len(sys.argv) > 1:
+    search_str = sys.argv[1]
+else:
+    search_str = input("Please enter a search term: ")
+    # search_str = 'Radiohead'
+print(type(search_str))
+
+sp = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials())
+result = sp.search(search_str)
+pprint.pprint(result)
+print(type(result))
+print(result.keys())
+
+
+## Step 1: user inputs genre
+# need to figure out which genres Spotify recognizes to match
+# fail gracefully and prompt user again if they enter a genre it doesn't recognize
+
+## Step 2: comp returns random selection of artists from genre
+# randomize artist list from genre
+
+## Step 3: user clicks links to Spotify
+# generate link to Spotify
