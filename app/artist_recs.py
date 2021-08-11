b9b70c1a8b935059f08133b965643a538df39a08
Sedina <sat9978l@nyu.edu>
2021-08-10 20:53:44 -0400

    Error Message Updated

--- app/artist_recs.py
+++ app/artist_recs.py
@@ -1,6 +1,7 @@
 from dotenv import load_dotenv # helps us access .env variables
 import os
 import spotipy
+import random
 from spotipy.oauth2 import SpotifyClientCredentials
 from pprint import pprint
 
@@ -11,78 +12,46 @@ load_dotenv() # load environment variables
 client_credentials_manager = SpotifyClientCredentials()
 client = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
 
-# USER SEARCHES FOR ARTIST
-search_term = input("Enter the name of a musical artist you like: ")
-response = client.search(q=search_term, type="artist", limit=20)
-
-artist_list = [] ## not sure if we need this list anymore
-
-print("ARTIST CHOICES:")
-for option in response["artists"]["items"]:
-    artist_list.append(option)
-    print(option["name"]) #would be nice to have list number next to artist so user doesn't have to re-type full name.
-
-###
-# mention = input("Please type your artist's name exactly as it appears above to confirm which one you were thinking of: ")
-
-# ## TRYING TO ADD ERROR MESSAGE BUT STRUGGLING
-# for option in response["artists"]["items"]:
-#     if option["name"] == mention:
-#         #print("SUCCESS!!")
-#         artists_id = option["id"]
-        # print(artists_id)
-    # else:
-    #    print("fail :(")
+def fetch_artists(search_term):
+    response = client.search(q=search_term, type="artist", limit=20)
+    return response["artists"]["items"]
+        
 
 
-###
-# for option in artist_list:
-#     if option["name"] == mention:
-#         #print("SUCCESS!!")
-#         artists_id = option["id"]
-#         # print(artists_id)
-#         break
+# Main conditional, can be indented
+if __name__ == "__main__":
+   
 
+    # USER SEARCHES FOR ARTIST
+    search_term = input("Please enter the name of a musical artist you like: ")
 
+    results = fetch_artists(search_term)
 
-### when user enter's a name not in the list > print("WHOOPS")
+    # artist_list = []
 
-# something like if artists_id is not defined or is an empty string > do something
-# maybe len(artists_id) = 0
+    print("ARTIST CHOICES:")
+    for artist_options in results:
+        # artist_list.append(artist_options)
+        print(artist_options["name"]) #would be nice to have list number next to artist so user doesn't have to re-type full name.
 
-# should loop back and allow the user to try inputing a name on the list again
+    mention = input("Please type to confirm the name of the artist you were thinking of: ")
 
-## WHILE VERSION
+    matching_artists = [artist for artist in results if artist["name"] == mention]
 
-while True:
-    mention = input("choose an artist name")
-    matching_artists = [opt for opt in artist_list if opt["name"] == mention]
     try:
-        matching_artist = matching_artists[0]
-        artists_id = option["id"]
-        print(artists_id)
-        break
-    except KeyError:
-        print("OOPS FOUND NO MATCHING ARTIST")
-        # pass
-
-# print(matching_artist) # do something with the matching artist
-# ### OG OPTION cycles through them one at a time
-# for option in response["artists"]["items"]:
-#     if option["name"] == mention:
-#         #print("SUCCESS!!")
-#         artists_id = option["id"]
-#         # print(artists_id)
-#     else:
-#        print("fail :(")
+        matching_artist = matching_artists[0] # triggers an IndexError (list index out of range)
+        artists_id = matching_artist["id"]
+    except IndexError:
+        print("OOPS - TRY AGAIN")
+        exit()
 
 
-# GETS US NAME WHEN ARTIST ID IS KNOWN
-new_response = client.artist_related_artists(artist_id=artists_id)
+    # GETS US NAME WHEN ARTIST ID IS KNOWN
+    new_response = client.artist_related_artists(artist_id=artists_id)
+    # response = client.artist_related_artists(artist_id="66CXWjxzNUsdJxJ2JdwvnR")
+    # pprint(new_response)
+    print("These are your artist recommendations:")
+    for a in new_response["artists"]:
+        print(a["name"], ":", a["external_urls"]["spotify"],"Popularity:", a["popularity"]) #Added popularity, need to understand what that is
 
-print("We think you'll like:")
-for a in new_response["artists"]:
-    print(a["name"], "|", a["external_urls"]["spotify"],"| Popularity:", a["popularity"]) #Added popularity, need to understand what that is
-
-# print("NEW RESPONSE: ", new_response)
-# print(type(new_response)) ## It's a dictionary
+    
